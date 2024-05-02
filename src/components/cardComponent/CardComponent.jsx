30c6bb676b4d1ff1af3bb8e188be9cef0c60ffc1
Nenubook <fernando.modesto.mendez@gmail.com>
2024-05-01 23:56:57 -0300

    cambios de card

--- src/components/cardComponent/CardComponent.jsx
+++ src/components/cardComponent/CardComponent.jsx
@@ -4,23 +4,25 @@ import {useEffect, useState} from 'react'
 import './CardComponent.css'
 
 
+
 const CardComponent = (items) => {
-  
+
   const [count, setCount] = useState(1)
-  const db= items.items
 
   return (
-     <div className='cardContainer'>
-       {items.items.map((item, index) => (
-         <div key={index} className="card">
-           <img src={item.img} alt={item.nombre} className='card__img' />
-           <p className='card__nombre_b'>{item.nombre}</p>
-           <p className='card__detalle_b'>{item.detalle}</p>
-           <p className='card__precio_b'>${item.precio}</p>
-           <BtnATC count={count} setCount={setCount}/>
-         </div>
-       ))}  
-     </div>
+    <div>
+      <div className='cardContainer'>
+        {items.items.map((item, index) => (
+          <div key={index} className="card">
+            <img src={item.img} alt={item.nombre} className='card__img' />
+            <p className='card__nombre_b'>{item.nombre}</p>
+            <p className='card__detalle_b'>{item.detalle}</p>
+            <p className='card__precio_b'>${item.precio}</p>
+            <BtnATC count={count} setCount={setCount}/>
+          </div>
+        ))}  
+      </div>
+    </div>
     
   )
 }
--- src/components/itemList/ItemList.jsx
+++ src/components/itemList/ItemList.jsx
@@ -1,11 +1,23 @@
+/* eslint-disable react/prop-types */
+import Header from '../header/Header.jsx'
+import Footer from '../footer/Footer.jsx'
+import BtnAddPedido from '../btnAddPedido/BtnAddPedido.jsx'
 import CardComponent from '../cardComponent/CardComponent.jsx'
 import './ItemList.css'
 
 // eslint-disable-next-line react/prop-types
 const ItemList = ({items}) => {
+  
   return (
     <div className='itemList'>
-      <CardComponent items={items}/> 
+      <Header/> 
+      <div>
+        {/* BTN VOLVER */}
+        <BtnAddPedido/>
+      </div>
+      <h2>Estas son nuestras opciones en {items[0].categoria}</h2>
+      <CardComponent  items={items}/> 
+      {/* <Footer/> */}
     </div>
   )
 }
