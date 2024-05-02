3bf7b09d3c12f04811180387be475d101a2669d4
Nenubook <fernando.modesto.mendez@gmail.com>
2024-05-01 22:58:58 -0300

    fixed render bug

--- src/components/cardComponent/CardComponent.jsx
+++ src/components/cardComponent/CardComponent.jsx
@@ -5,23 +5,23 @@ import './CardComponent.css'
 
 
 const CardComponent = (items) => {
+  
   const [count, setCount] = useState(1)
-  
-  console.log(items.items)
-  
+  const db= items.items
+
   return (
-    <div className='cardContainer'>
-      {items.items.map((item, index) => (
-        <div key={index} className="card">
-          <img src={item.img} alt={item.nombre} className='card__img' />
-          <p className='card__nombre_b'>{item.nombre}</p>
-          <p className='card__detalle_b'>{item.detalle}</p>
-          <p className='card__precio_b'>${item.precio}</p>
-          <BtnATC count={count} setCount={setCount}/>
-        </div>
-      ))}  
-    </div>
-    //<></>
+     <div className='cardContainer'>
+       {items.items.map((item, index) => (
+         <div key={index} className="card">
+           <img src={item.img} alt={item.nombre} className='card__img' />
+           <p className='card__nombre_b'>{item.nombre}</p>
+           <p className='card__detalle_b'>{item.detalle}</p>
+           <p className='card__precio_b'>${item.precio}</p>
+           <BtnATC count={count} setCount={setCount}/>
+         </div>
+       ))}  
+     </div>
+    
   )
 }
 
--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-unused-vars */
 import './ItemListContainer.css'
 import { useState, useEffect } from 'react'
 import { useParams } from 'react-router-dom'
@@ -6,7 +5,6 @@ import ItemList from '../itemList/ItemList.jsx'
 
 const ItemListContainer = () => {
     const [items , setItems] = useState();
-    const [db, setDb] = useState();
     const {categoria} = useParams();
     const url = `https://api-menu-six.vercel.app/api/categoria/${categoria}/`
 
@@ -18,26 +16,22 @@ const ItemListContainer = () => {
                   throw new Error('Network response was not ok');
               }
               const data = await response.json();
-              const db = data.payload
-              setItems(db)
-              ; // Establecer los datos obtenidos en el estado
+              const db = data.payload;
+              setItems(db);
           } catch (error) {
               console.error('Error fetching data:', error);
           }
       };
-      fetchData(); // Llamar a la función de búsqueda de datos cuando el componente se monta
+
+      fetchData(); 
     }, [url]);
 
-    useEffect(()=>{
-      setDb(items)
-    }, [items])
-
-    
-  return (
-    <div>
-      <ItemList items={db}/>
-    </div>
-  )
+    // Renderizar ItemList solo cuando los items se han cargado correctamente
+    return (
+        <div>
+            {items && <ItemList items={items}/>}
+        </div>
+    );
 }
 
-export default ItemListContainer
+export default ItemListContainer;
