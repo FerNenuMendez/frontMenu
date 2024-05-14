ca00830a7cf6cacecc3372e5e9cdfabd78e5262d
Fernando Mendez <fernando.modesto.mendez@gmail.com>
2024-05-14 19:19:00 -0300

    prueba de QR 1

--- src/pages/index/Index.jsx
+++ src/pages/index/Index.jsx
@@ -1,18 +1,25 @@
 import Header from '../../components/header/Header.jsx'
 import Footer from '../../components/footer/Footer.jsx'
-import { useContext } from "react";
-import { Link } from "react-router-dom"
+import { useState, useContext, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
 import { cartContext } from '../../context/cartProvider.jsx';
 import './Index.css';
 
 function Index() {
   //context pedido
-  const {infoMesa} = useContext(cartContext)
-  //funcion que muestra el menu y guarda info de la mesa
+  const {infoMesa} = useContext(cartContext);
+  const { mesa } = useParams(); // Obtener el parámetro "mesa" de la URL
+  const [mesaValue, setMesaValue] = useState(mesa || ""); // Establecer el estado inicial con el valor del parámetro "mesa"
+
+  useEffect(() => {
+    // Actualizar el valor del campo de entrada "mesa" cuando cambia el parámetro en la URL
+    setMesaValue(mesa || "");
+  }, [mesa]);
+
   function verMenu() {
     const nombre = document.getElementById('nombre').value;
-    const mesaNum = parseInt(document.getElementById('mesa').value, 10);
-    infoMesa(nombre, mesaNum)
+    const mesaNum = parseInt(mesaValue, 10); // Usar el valor del estado para la mesa
+    infoMesa(nombre, mesaNum);
   }
 
   return (
